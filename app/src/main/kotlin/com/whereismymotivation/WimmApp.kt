25efabf1988bf7b22545d9e23386eb3adc00903e
Janishar Ali <janishar.ali@gmail.com>
2023-12-14 03:53:45 +0530

    add fcm token fetcher

--- app/src/main/kotlin/com/whereismymotivation/WimmApp.kt
+++ app/src/main/kotlin/com/whereismymotivation/WimmApp.kt
@@ -6,10 +6,13 @@ import androidx.work.Configuration
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
 import com.whereismymotivation.analytics.Tracker
 import com.whereismymotivation.data.repository.AppMetricRepository
 import com.whereismymotivation.data.repository.UserRepository
 import com.whereismymotivation.utils.common.SystemUtils
+import com.whereismymotivation.utils.display.Toaster
+import com.whereismymotivation.utils.log.Logger
 import com.whereismymotivation.work.AppAlarmManager
 import com.whereismymotivation.work.AppWorkManager
 import dagger.hilt.android.HiltAndroidApp
@@ -17,6 +20,10 @@ import javax.inject.Inject
 
 @HiltAndroidApp
 class WimmApp : Application(), Configuration.Provider {
+
+    companion object {
+        val TAG = "WimmApp"
+    }
 
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
@@ -41,6 +48,7 @@ class WimmApp : Application(), Configuration.Provider {
         appMetricRepository.setCurrentAppVersion(SystemUtils.getAppVersionCode(this))
         recordUser()
         scheduleWorks()
+        getFcmToken()
     }
 
     private fun recordUser() {
@@ -49,6 +57,22 @@ class WimmApp : Application(), Configuration.Provider {
             Firebase.analytics.setUserId(userId)
             userEmail?.let { Firebase.analytics.setUserProperty("Email", it) }
             userName?.let { Firebase.analytics.setUserProperty("Name", it) }
+        }
+    }
+
+    private fun getFcmToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Logger.e(
+                    TAG,
+                    "Fetching FCM registration token failed",
+                    task.exception.toString()
+                )
+                return@addOnCompleteListener
+            }
+            val token = task.result
+            Logger.d(TAG, token)
+            Toaster.show(applicationContext, token)
         }
     }
 
