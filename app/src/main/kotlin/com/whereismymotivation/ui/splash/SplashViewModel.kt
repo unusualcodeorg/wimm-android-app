fb1a9819f05f7be2226e165454b831e355c8ea31
Janishar Ali <janishar.ali@gmail.com>
2023-12-16 16:25:38 +0530

    integrate login api

--- app/src/main/kotlin/com/whereismymotivation/ui/splash/SplashViewModel.kt
+++ app/src/main/kotlin/com/whereismymotivation/ui/splash/SplashViewModel.kt
@@ -1,16 +1,20 @@
 package com.whereismymotivation.ui.splash
 
+import androidx.lifecycle.viewModelScope
+import com.whereismymotivation.data.repository.UserRepository
 import com.whereismymotivation.ui.base.BaseViewModel
 import com.whereismymotivation.ui.navigation.Destination
 import com.whereismymotivation.ui.navigation.NavTarget
 import com.whereismymotivation.ui.navigation.Navigator
 import com.whereismymotivation.utils.network.NetworkHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
     networkHelper: NetworkHelper,
+    private val userRepository: UserRepository,
     val navigator: Navigator,
 ) : BaseViewModel(networkHelper) {
 
@@ -19,6 +23,14 @@ class SplashViewModel @Inject constructor(
     }
 
     fun animationComplete() {
-        navigator.navigateTo(NavTarget(Destination.Login, true))
+        viewModelScope.launch {
+            val exists = userRepository.userExists()
+            if (exists) {
+                navigator.navigateTo(NavTarget(Destination.Home.Feed, true))
+            } else {
+                navigator.navigateTo(NavTarget(Destination.Login, true))
+            }
+        }
+
     }
 }
