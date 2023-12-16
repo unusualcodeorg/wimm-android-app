4214ee591ea98ee73af6dcdf7f941e9765270352
Janishar Ali <janishar.ali@gmail.com>
2023-12-16 16:40:59 +0530

    integrate logout api

--- app/src/main/kotlin/com/whereismymotivation/ui/profile/Profile.kt
+++ app/src/main/kotlin/com/whereismymotivation/ui/profile/Profile.kt
@@ -1,14 +1,28 @@
 package com.whereismymotivation.ui.profile
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun Profile(modifier: Modifier) {
+fun Profile(modifier: Modifier, viewModel: ProfileViewModel) {
+    ProfileView(modifier = modifier) { viewModel.logout() }
+}
+
+@Composable
+private fun ProfileView(modifier: Modifier, logout: () -> Unit) {
     Box(modifier = modifier) {
-        Text(text = "Profile", style = MaterialTheme.typography.headlineLarge)
+        Button(onClick = logout) {
+            Text(text = "Logout")
+        }
     }
+}
+
+@Preview
+@Composable
+private fun ProfilePreview() {
+    ProfileView(modifier = Modifier) {}
 }
