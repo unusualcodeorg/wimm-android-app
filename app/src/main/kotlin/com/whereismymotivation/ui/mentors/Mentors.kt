f59795d48df03fa2fd447bf9a4fddc84df2e4cd5
Janishar Ali <janishar.ali@gmail.com>
2023-12-17 04:18:14 +0530

    add mentors appbar

--- app/src/main/kotlin/com/whereismymotivation/ui/mentors/Mentors.kt
+++ app/src/main/kotlin/com/whereismymotivation/ui/mentors/Mentors.kt
@@ -2,6 +2,7 @@ package com.whereismymotivation.ui.mentors
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,16 +16,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.whereismymotivation.R
 import com.whereismymotivation.data.model.Mentor
 import com.whereismymotivation.ui.common.image.NetworkImage
 import com.whereismymotivation.ui.common.image.OutlinedAvatar
+import com.whereismymotivation.ui.home.HomeAppBar
 import com.whereismymotivation.ui.theme.AppTheme
 import java.util.Locale
 
@@ -47,18 +50,20 @@ fun MentorsView(
     modifier: Modifier = Modifier,
     selectMentor: (Mentor) -> Unit,
 ) {
-    val configuration = LocalConfiguration.current
-    LazyVerticalStaggeredGrid(
-        columns = StaggeredGridCells.Fixed(2),
-        verticalItemSpacing = 4.dp,
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-        content = {
-            items(mentors) { mentor ->
-                MentorView(mentor, selectMentor)
-            }
-        },
-        modifier = Modifier.fillMaxWidth()
-    )
+    Column(modifier = modifier) {
+        HomeAppBar(title = stringResource(R.string.my_mentors))
+        LazyVerticalStaggeredGrid(
+            columns = StaggeredGridCells.Fixed(2),
+            verticalItemSpacing = 2.dp,
+            horizontalArrangement = Arrangement.spacedBy(2.dp),
+            content = {
+                items(mentors) { mentor ->
+                    MentorView(mentor, selectMentor)
+                }
+            },
+            modifier = Modifier.fillMaxWidth().padding(4.dp)
+        )
+    }
 }
 
 @Composable
