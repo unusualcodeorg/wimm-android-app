1f763a81ad0024399d1a7c8fedf7690cd1bcc964
Janishar Ali <janishar.ali@gmail.com>
2023-12-17 00:43:20 +0530

    add loader and improve network error

---
+++ app/src/main/kotlin/com/whereismymotivation/ui/common/progress/Loading.kt
@@ -0,0 +1,20 @@
+package com.whereismymotivation.ui.common.progress
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Loading() {
+//    var loading by remember { mutableStateOf(false) }
+//
+//    Button(onClick = { loading = true }, enabled = !loading) {
+//        Text("Start loading")
+//    }
+//
+//    if (!loading) return
+//
+//    CircularProgressIndicator(
+//        modifier = Modifier.width(64.dp),
+//        color = MaterialTheme.colorScheme.secondary,
+//        trackColor = MaterialTheme.colorScheme.surfaceVariant,
+//    )
+}
