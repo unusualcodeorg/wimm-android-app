dd792483b224b97b77b588784faebd1b0ed920e3
Janishar Ali <janishar.ali@gmail.com>
2023-12-17 01:42:19 +0530

    restructure code

--- app/src/main/kotlin/com/whereismymotivation/ui/home/Home.kt
+++ app/src/main/kotlin/com/whereismymotivation/ui/home/Home.kt
@@ -2,35 +2,10 @@ package com.whereismymotivation.ui.home
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.add
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.windowInsetsBottomHeight
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import com.whereismymotivation.R
 import com.whereismymotivation.ui.feed.Feed
 import com.whereismymotivation.ui.mentors.Mentors
@@ -56,80 +31,8 @@ fun NavGraphBuilder.home(
         val viewModel: ProfileViewModel = hiltViewModel()
         Profile(modifier, viewModel)
     }
-
     composable(Destination.Home.MyBox.route) {
         MyBox(modifier)
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun HomeAppBar() {
-    TopAppBar(
-        title = {},
-        modifier = Modifier.height(80.dp),
-        actions = {
-            Image(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .align(Alignment.CenterVertically),
-                painter = painterResource(id = R.drawable.wimm_logo),
-                contentDescription = null
-            )
-            IconButton(
-                modifier = Modifier.align(Alignment.CenterVertically),
-                onClick = { /* todo */ }
-            ) {
-                Icon(
-                    imageVector = Icons.Filled.AccountCircle,
-                    contentDescription = stringResource(R.string.my_profile)
-                )
-            }
-        }
-    )
-}
-
-@Composable
-fun HomeBottomBar(navController: NavController) {
-    val tabs = remember { HomeTab.entries.toTypedArray() }
-
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentRoute = navBackStackEntry?.destination?.route
-        ?: Destination.Home.Feed.route
-
-    val routes = remember { HomeTab.entries.map { it.route } }
-    if (currentRoute in routes) {
-        NavigationBar(
-            Modifier.windowInsetsBottomHeight(
-                WindowInsets.navigationBars.add(WindowInsets(bottom = 56.dp))
-            )
-        ) {
-            tabs.forEach { tab ->
-                val selected = currentRoute == tab.route
-                NavigationBarItem(
-                    icon = {
-                        Icon(
-                            painterResource(if (selected) tab.selectedIcon else tab.unselectedIcon),
-                            contentDescription = null
-                        )
-                    },
-                    selected = selected,
-                    onClick = {
-                        if (tab.route != currentRoute) {
-                            navController.navigate(tab.route) {
-                                popUpTo(navController.graph.startDestinationId) {
-                                    saveState = true
-                                }
-                                launchSingleTop = true
-                                restoreState = true
-                            }
-                        }
-                    },
-                    alwaysShowLabel = false,
-                    modifier = Modifier.navigationBarsPadding()
-                )
-            }
-        }
     }
 }
 
