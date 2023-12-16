5e8b12da6524ec2c5195e529a2ee871cfe230efb
Janishar Ali <janishar.ali@gmail.com>
2023-12-17 04:18:14 +0530

    add mentors appbar

---
+++ app/src/main/kotlin/com/whereismymotivation/ui/home/HomeAppBar.kt
@@ -0,0 +1,53 @@
+package com.whereismymotivation.ui.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.whereismymotivation.R
+
+@Composable
+fun HomeAppBar(modifier: Modifier = Modifier, title: String) {
+    Surface(
+        modifier = modifier.height(80.dp)
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+                .height(56.dp),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.CenterVertically),
+                painter = painterResource(id = R.drawable.wimm_logo),
+                contentDescription = null
+            )
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun HomeAppBarPreview() {
+    HomeAppBar(title = stringResource(R.string.app_name))
+}
