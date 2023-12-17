c07ecb9e3764f36136384ae7cc4a83b5cdf41421
Janishar Ali <janishar.ali@gmail.com>
2023-12-18 04:53:56 +0530

    clear up

--- app/src/main/kotlin/com/whereismymotivation/ui/search/Search.kt
+++ app/src/main/kotlin/com/whereismymotivation/ui/search/Search.kt
@@ -1,6 +1,7 @@
 package com.whereismymotivation.ui.search
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,7 +9,7 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun Search(modifier: Modifier) {
-    Box(modifier = modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
         Text(text = "Search", style = MaterialTheme.typography.headlineLarge)
     }
 }
