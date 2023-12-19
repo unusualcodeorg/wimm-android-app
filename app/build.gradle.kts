8d165a0faf2b37437c8274ae42dbf0d3671ce8ab
Janishar Ali <janishar.ali@gmail.com>
2023-12-20 04:11:12 +0530

    update library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,7 @@ dependencies {
     implementation("androidx.compose.material3:material3-window-size-class")
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
     // Coroutine
