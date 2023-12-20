504db8b4222a62666d84e55b51f128af738acc36
Janishar Ali <janishar.ali@gmail.com>
2023-12-20 06:07:08 +0530

    add remote config initialization

--- app/src/main/kotlin/com/whereismymotivation/data/repository/RemoteConfigRepository.kt
+++ app/src/main/kotlin/com/whereismymotivation/data/repository/RemoteConfigRepository.kt
@@ -1,27 +1,20 @@
 package com.whereismymotivation.data.repository
 
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.whereismymotivation.utils.config.RemoteKey.HOME_FEED_LAST_SEEN_REFRESH_DAYS
+import com.whereismymotivation.utils.config.RemoteKey.HOME_PAGE_CONTENT_COUNT
+import com.whereismymotivation.utils.config.RemoteKey.INVITE_FRIEND_MESSAGE
+import com.whereismymotivation.utils.config.RemoteKey.RATING_DIALOG_LATER_TO_SHOW
+import com.whereismymotivation.utils.config.RemoteKey.RATING_DIALOG_MAX_NUM_OF_TIMES_SHOW_RATING_IN_CURRENT_APP_VERSION
+import com.whereismymotivation.utils.config.RemoteKey.RATING_DIALOG_MAX_RATING
+import com.whereismymotivation.utils.config.RemoteKey.RATING_DIALOG_MIN_GAP_APP_OPEN_SHOW_RATING_COUNT
+import com.whereismymotivation.utils.config.RemoteKey.SHARE_WATERMARK_TEXT
+import com.whereismymotivation.utils.config.RemoteKey.YOUTUBE_API_KEY
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class RemoteConfigRepository @Inject constructor(private val config: FirebaseRemoteConfig) {
-
-    companion object {
-        private const val HOME_PAGE_CONTENT_COUNT = "home_page_content_count"
-        private const val INVITE_FRIEND_MESSAGE = "invite_friend_message"
-        private const val SHARE_WATERMARK_TEXT = "share_water_mark_text"
-        private const val YOUTUBE_API_KEY = "youtube_api_key"
-        private const val HOME_FEED_LAST_SEEN_REFRESH_DAYS = "home_feed_last_seen_refresh_days"
-        private const val RATING_DIALOG_LATER_TO_SHOW = "rating_dialog_later_to_show"
-
-        private const val RATING_DIALOG_MAX_RATING =
-            "rating_dialog_max_rating"
-        private const val RATING_DIALOG_MIN_GAP_APP_OPEN_SHOW_RATING_COUNT =
-            "rating_dialog_min_gap_app_open_count"
-        private const val RATING_DIALOG_MAX_NUM_OF_TIMES_SHOW_RATING_IN_CURRENT_APP_VERSION =
-            "rating_dialog_max_show_count_in_current_version"
-    }
 
     fun getHomePageContentCount() = config.getLong(HOME_PAGE_CONTENT_COUNT).toInt()
     fun getPageContentCount() = config.getLong(HOME_PAGE_CONTENT_COUNT).toInt()
