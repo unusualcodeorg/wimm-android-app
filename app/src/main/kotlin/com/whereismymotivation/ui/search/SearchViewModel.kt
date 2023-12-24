7fe00a0cf31e5ebaaeed74920c67d9e93aae972a
Janishar Ali <janishar.ali@gmail.com>
2023-12-24 21:46:50 +0530

    add search query handler

--- app/src/main/kotlin/com/whereismymotivation/ui/search/SearchViewModel.kt
+++ app/src/main/kotlin/com/whereismymotivation/ui/search/SearchViewModel.kt
@@ -1,24 +1,40 @@
 package com.whereismymotivation.ui.search
 
+import androidx.lifecycle.viewModelScope
 import com.whereismymotivation.data.model.UniversalSearchResult
+import com.whereismymotivation.data.repository.SearchRepository
 import com.whereismymotivation.ui.base.BaseViewModel
 import com.whereismymotivation.ui.common.progress.Loader
 import com.whereismymotivation.ui.common.snackbar.Messenger
 import com.whereismymotivation.utils.network.NetworkHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
+@OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
 @HiltViewModel
 class SearchViewModel @Inject constructor(
     networkHelper: NetworkHelper,
     loader: Loader,
-    messenger: Messenger
+    messenger: Messenger,
+    searchRepository: SearchRepository
 ) : BaseViewModel(networkHelper, loader, messenger) {
 
     companion object {
         const val TAG = "SearchViewModel"
+        private var searchMode: SearchMode = SearchMode.SEARCH_MODE_UNIVERSAL
     }
 
     val _results = MutableStateFlow<List<UniversalSearchResult>>(emptyList())
@@ -27,9 +43,36 @@ class SearchViewModel @Inject constructor(
     val results = _results.asStateFlow()
     val query = _query.asStateFlow()
 
+    private val queryFlow = MutableSharedFlow<String>(
+        replay = 0,
+        extraBufferCapacity = 1,
+        BufferOverflow.DROP_OLDEST
+    )
+
+    init {
+        queryFlow.debounce(500)
+            .distinctUntilChanged()
+            .mapLatest {
+                if (it.isEmpty() || it.isBlank())
+                    return@mapLatest flowOf(emptyList<UniversalSearchResult>())
+                return@mapLatest when (searchMode) {
+                    SearchMode.SEARCH_MODE_UNIVERSAL -> searchRepository.fetchSearchResults(it)
+                    SearchMode.SEARCH_MODE_MENTOR -> searchRepository.fetchMentorSearchResults(it)
+                    SearchMode.SEARCH_MODE_TOPIC -> searchRepository.fetchTopicSearchResults(it)
+                }
+            }
+            .onEach {
+                it.collect { result ->
+                    _results.value = result
+                }
+            }
+            .catch { loader.stop() }
+            .launchIn(viewModelScope)
+    }
+
     fun search(query: String) {
         _query.value = query
-
+        queryFlow.tryEmit(query)
     }
 
     fun selectResult(result: UniversalSearchResult) {}
