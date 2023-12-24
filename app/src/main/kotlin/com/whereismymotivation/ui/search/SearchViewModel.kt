e859a199b0430a92f5813f205aa8772fb9605772
Janishar Ali <janishar.ali@gmail.com>
2023-12-24 21:09:19 +0530

    add search view model

--- app/src/main/kotlin/com/whereismymotivation/ui/search/SearchViewModel.kt
+++ app/src/main/kotlin/com/whereismymotivation/ui/search/SearchViewModel.kt
@@ -1,10 +1,13 @@
 package com.whereismymotivation.ui.search
 
+import com.whereismymotivation.data.model.UniversalSearchResult
 import com.whereismymotivation.ui.base.BaseViewModel
 import com.whereismymotivation.ui.common.progress.Loader
 import com.whereismymotivation.ui.common.snackbar.Messenger
 import com.whereismymotivation.utils.network.NetworkHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,4 +20,18 @@ class SearchViewModel @Inject constructor(
     companion object {
         const val TAG = "SearchViewModel"
     }
+
+    val _results = MutableStateFlow<List<UniversalSearchResult>>(emptyList())
+    val _query = MutableStateFlow("")
+
+    val results = _results.asStateFlow()
+    val query = _query.asStateFlow()
+
+    fun search(query: String) {
+        _query.value = query
+
+    }
+
+    fun selectResult(result: UniversalSearchResult) {}
+
 }
