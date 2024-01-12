db3c8c2ac46a47a154204a8d5e7c32b7a40d4faf
Janishar Ali <janishar.ali@gmail.com>
2024-01-13 04:24:00 +0530

    merge fix

--- app/src/main/kotlin/com/whereismymotivation/ui/common/preview/Provider.kt
+++ app/src/main/kotlin/com/whereismymotivation/ui/common/preview/Provider.kt
@@ -34,20 +34,6 @@ class TopicPreviewParameterProvider : PreviewParameterProvider<Topic> {
     )
 }
 
-class TopicPreviewParameterProvider : PreviewParameterProvider<Topic> {
-    override val values = sequenceOf(
-        Topic(
-            id = "abc",
-            name = "Computer Guy",
-            thumbnail = "https://upload.wikimedia.org/wikipedia/en/c/cb/Osho.jpg",
-            title = "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            coverImgUrl = "https://cdn.pixabay.com/photo/2017/09/12/11/56/universe-2742113_1280.jpg",
-            description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget lorem dolor sed viverra ipsum nunc aliquet bibendum. Et netus et malesuada fames ac turpis egestas maecenas pharetra. Facilisis gravida neque convallis a cras semper. \n\n Maecenas ultricies mi eget mauris pharetra et ultrices neque. Et odio pellentesque diam volutpat commodo sed. Et ultrices neque ornare aenean euismod. Ac feugiat sed lectus vestibulum mattis ullamcorper velit. Ut tristique et egestas quis ipsum \n\n suspendisse ultrices gravida. Vel orci porta non pulvinar neque. Laoreet sit amet cursus sit amet dictum. Nibh tellus molestie nunc non blandit massa enim nec dui.",
-            subscribed = false,
-        )
-    )
-}
-
 class ContentPreviewParameterProvider : PreviewParameterProvider<Content> {
     override val values = sequenceOf(
         Content(
