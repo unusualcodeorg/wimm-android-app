4824fa21dca829559b2484c8960c3c6c599be6f6
Janishar Ali <janishar.ali@gmail.com>
2024-01-21 06:02:11 +0530

    remove unused di

--- app/src/main/kotlin/com/whereismymotivation/di/component/BroadcastComponent.kt
+++
@@ -1,9 +0,0 @@
-package com.whereismymotivation.di.component
-
-import com.whereismymotivation.di.scope.BroadcastScope
-import dagger.hilt.DefineComponent
-import dagger.hilt.components.SingletonComponent
-
-@BroadcastScope
-@DefineComponent(parent = SingletonComponent::class)
-interface BroadcastComponent
