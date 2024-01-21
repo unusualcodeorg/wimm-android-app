4a6d3e7ae60728d88e05994f979c660347bae16e
Janishar Ali <janishar.ali@gmail.com>
2024-01-21 17:19:23 +0530

    improve alarm manager code

--- app/src/main/kotlin/com/whereismymotivation/work/AppAlarmManager.kt
+++ app/src/main/kotlin/com/whereismymotivation/work/AppAlarmManager.kt
@@ -30,15 +30,20 @@ class AppAlarmManager @Inject constructor(
             val alarmManager =
                 context.getSystemService(Context.ALARM_SERVICE) as AlarmManager? ?: return
 
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+                val canSchedule = alarmManager.canScheduleExactAlarms()
+                if (!canSchedule) return
+            }
+
             val calendarNow = Calendar.getInstance()
-            val calendar8 = Calendar.getInstance().apply {
+            val calendarExact = Calendar.getInstance().apply {
                 set(
                     get(Calendar.YEAR), get(Calendar.MONTH), get(Calendar.DAY_OF_MONTH),
                     5, 22, 0
                 )
             }
 
-            if (calendarNow.after(calendar8)) calendar8.add(Calendar.DAY_OF_MONTH, 1)
+            if (calendarNow.after(calendarExact)) calendarExact.add(Calendar.DAY_OF_MONTH, 1)
 
             val intent = Intent(context, DailyMoodAlarmReceiver::class.java).apply {
                 action = ACTION_DAILY_MOOD_RECORD
@@ -51,31 +56,10 @@ class AppAlarmManager @Inject constructor(
                 PendingIntent.FLAG_IMMUTABLE
             )
 
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
-                val canSchedule = alarmManager.canScheduleExactAlarms()
-                if (canSchedule) {
-                    scheduleAlarm(alarmManager, calendar8.timeInMillis, pIntent)
-                } else {
-                    // TODO: For Testing: Move it at appropriate place
-//                    val permissionIntent = Intent(ACTION_REQUEST_SCHEDULE_EXACT_ALARM)
-//                        .apply {
-//                            flags = Intent.FLAG_ACTIVITY_NEW_TASK
-//                        }
-//                    context.startActivity(permissionIntent)
-                }
-            } else {
-                scheduleAlarm(alarmManager, calendar8.timeInMillis, pIntent)
-            }
+            alarmManager.setExact(AlarmManager.RTC, calendarExact.timeInMillis, pIntent)
+
         } catch (e: Exception) {
             Logger.record(e)
         }
-    }
-
-    private fun scheduleAlarm(
-        alarmManager: AlarmManager,
-        mills: Long,
-        intent: PendingIntent
-    ) {
-        alarmManager.setExact(AlarmManager.RTC, mills, intent)
     }
 }
